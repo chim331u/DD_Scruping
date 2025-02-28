a052d11e8a1a281e7f1ad7c385c5631d41508424
Monti Luca <luca.monti@corner.ch>
2025-02-28 17:11:28 +0100

    Added one-to-many

--- LinkEd2k.cs
+++ LinkEd2k.cs
@@ -5,5 +5,7 @@ public class LinkEd2k : BaseEntity
     public int Id { get; set; }
     public string Ed2kLink { get; set; }
     public string Title { get; set; }
-    
+
+    public int ThreadsId { get; set; } // Required foreign key property
+    public Threads Threads { get; set; }  // Required reference navigation to principal
 }
--- Threads.cs
+++ Threads.cs
@@ -7,4 +7,16 @@ public class Threads : BaseEntity
     public string? MainTitle { get; set; }
     public string? Type { get; set; }
     
+    public ICollection<LinkEd2k> LinkEd2Ks { get; } = new List<LinkEd2k>(); // Collection navigation containing dependents
+    
+    
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<Threads>()
+    //         .HasMany(e => e.LinkEd2k)
+    //         .WithOne(e => e.Threads)
+    //         .HasForeignKey(e => e.ThreadsId)
+    //         .IsRequired();
+    // }
+    
 }
